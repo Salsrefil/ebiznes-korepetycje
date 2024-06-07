96bda3022767ac0225ba80452bae045197bb7dd1
Salsrefil <240789@edu.p.lodz.pl>
2024-06-07 13:58:10 +0200

    Poprawa routingu (Firebase)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 //import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Jobs from './components/Jobs';
 import SaveJobs from './components/SaveJobs';
 import Login from './components/Login';
+import { auth } from './firebase.js';
 
 
 function App() {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in, print user info to console
+        console.log('User info:', user);
+      } else {
+        // User is signed out
+        console.log('No user is signed in.');
+      }
+    });
+
+    // Clean up the observer on component unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
