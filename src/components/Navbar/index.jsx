5d4015f5b2e744e7f4be4967ccffb85ea7381fc1
Salsrefil <240789@edu.p.lodz.pl>
2024-06-20 21:08:22 +0200

    Logout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,33 +1,59 @@
-import React, {useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import "./index.css";
 
 const Navbar = () => {
-  return (
-    <>
-      <div className="main-page">
-        <nav id="navbar">
-          <h1 className="logo">
-            Korki<span>.pl</span>
-          </h1>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = getAuth();
 
-          <ul>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [auth]);
+
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      setIsLoggedIn(false);
+    });
+  };
+
+  return (
+    <div className="main-page">
+      <nav id="navbar">
+        <h1 className="logo">
+          Korki<span>.pl</span>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/home">Strona Główna</Link>
+          </li>
+          <li>
+            <Link to="/jobs">Ogłoszenia korepetycji</Link>
+          </li>
+          <li>
+            <Link to="/post-tutoring">Wystaw ogłoszenie</Link>
+          </li>
+          {isLoggedIn ? (
             <li>
-              <Link to="/home">Strona Główna</Link>
+              <Link to="/" onClick={handleLogout}>Wyloguj</Link>
             </li>
-            <li>
-              <Link to="/jobs">Ogłoszenia korepetycji</Link>
-            </li>
-            <li>
-              <Link to="/post-tutoring">Wystaw ogłoszenie</Link>
-            </li>
+          ) : (
             <li>
               <Link to="/signin">Logowanie/Rejestracja</Link>
             </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+          )}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
